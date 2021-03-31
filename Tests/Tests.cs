c4c420f4cd927bccac71f27a83e82aa0972e14d2
aidanagul10 <aidanagul10@gmail.com>
2021-03-31 23:19:09 +0500

    Изменен тест и добавлен тест проверки даты

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -13,36 +13,56 @@ namespace Tests
         public void Setup()
         { }
 
-        [Test]
-        public void SimpleDateFilterTest()
+        ////[Test]
+        //public void SimpleDateFilterTest()
+        //{
+        //    Random rand = new Random();
+        //    DateTime from = new DateTime
+        //        (
+        //        rand.Next(2020, 2022),
+        //        rand.Next(1, 13),
+        //        rand.Next(1, 25)
+        //        );
+        //    DateTime to = from.AddDays(2);
+        //    DebtorMessageType type = BankrotClient.SupportedMessageTypes.First();
+        //    var messages = BankrotClient.GetMessages(from, to, type);
+        //    if (messages.Length != 0)
+        //    {
+        //        Assert.GreaterOrEqual(messages.Select(x => x.DatePublished.Date).Min(), from);
+        //        Assert.LessOrEqual(messages.Select(x => x.DatePublished.Date).Max(), to);
+        //    }
+
+        //}
+       //[TestCase(2021, 3, 1,ExpectedResult = 727)]
+        [TestCase(2020, 12, 10, ExpectedResult = 696)]
+        //[TestCase(2021, 2, 23, ExpectedResult = 163)]
+        public int TestGetMessages(int y, int m, int d)
         {
-            Random rand = new Random();
-            DateTime from = new DateTime
-                (
-                rand.Next(2020, 2022),
-                rand.Next(1, 13),
-                rand.Next(1, 25)
-                );
-            DateTime to = from.AddDays(2);
-            DebtorMessageType type = BankrotClient.SupportedMessageTypes.First();
-            var messages = BankrotClient.GetMessages(from, to, type);
-            if (messages.Length != 0)
-            {
-                Assert.GreaterOrEqual(messages.Select(x => x.DatePublished.Date).Min(), from);
-                Assert.LessOrEqual(messages.Select(x => x.DatePublished.Date).Max(), to);
-            }
+            var type = new DebtorMessageType() { Id = 19, Name = "Реализация имущества должника-банкрота" };
+            
+            DebtorMessage[] messages = BankrotClient.GetMessages(new DateTime(y, m, d), type);
+            return messages.Length;
+        }
+        [Test]
+        public void TestInputInvalidDate()
+        {
+            var start = new DateTime(2021, 3, 13);
+            var end = new DateTime(2021, 3, 12);
+            var type = new DebtorMessageType() { Id = 19, Name = "Реализация имущества должника-банкрота" };
+            var ex = Assert.Throws<Exception>(() => BankrotClient.GetMessages(start, end, type));
+
+            Assert.That(ex.Message == "Дата конца поиска не может быть раньше даты начала!");
 
         }
         [Test]
-        public void TestGetMessages()
+        public void TestInputInvalidInterval()
         {
-            var start = new DateTime(2021, 3, 1);
-            var end = new DateTime(2021, 3, 1);
-            var type = new DebtorMessageType(){Id = 19, Name = "Реализация имущества должника-банкрота"};
-            var leng = 727;
+            var start = new DateTime(2021, 1, 13);
+            var end = new DateTime(2021, 3, 12);
+            var type = new DebtorMessageType() { Id = 19, Name = "Реализация имущества должника-банкрота" };
+            var ex = Assert.Throws<InvalidOperationException>(() => BankrotClient.GetMessages(start, end, type));
 
-            DebtorMessage[] messages = BankrotClient.GetMessages(start, end, type);
-            Assert.AreEqual(leng, messages.Length);
+            Assert.That(ex.Message == "Максимальная длина интервала - 30 дней!");
         }
         [Test]
         public void TestExportMessagesToExcel()
