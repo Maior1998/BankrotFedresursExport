b00e3d7442b3a21c046c3d89ba2e6cfeeaebac93
Sergey Patoka <sergey.pat98@gmail.com>
2021-04-15 07:40:51 +0500

    Исправлена ошибка, Из-за которой неверно выставлялсмя интервал при поиске сообщения

--- BankruptFedresursClient/BankrotClient.cs
+++ BankruptFedresursClient/BankrotClient.cs
@@ -242,8 +242,8 @@ namespace BankruptFedresursClient
             "MessageNumber=" +
             "&MessageType=ArbitralDecree" +
             "&MessageTypeText=%d0%a1%d0%be%d0%be%d0%b1%d1%89%d0%b5%d0%bd%d0%b8%d0%b5+%d0%be+%d1%81%d1%83%d0%b4%d0%b5%d0%b1%d0%bd%d0%be%d0%bc+%d0%b0%d0%ba%d1%82%d0%b5" +
-            "&DateEndValue={0}+0%3a00%3a00" +
-            "&DateBeginValue={1}+0%3a00%3a00" +
+            "&DateEndValue={1}+0%3a00%3a00" +
+            "&DateBeginValue={0}+0%3a00%3a00" +
             "&PageNumber={3}" +
             "&DebtorText=" +
             "&DebtorId=" +
